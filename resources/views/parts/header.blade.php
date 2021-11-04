45844a122ea5eb39fc404b4675d756792a867bc3
rarsyansyahr <rarsyansyahr@gmail.com>
2021-11-04 08:38:36 +0700

    backup

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -39,7 +39,7 @@
     <header id="header" class="fixed-top ">
         <div class="container d-flex align-items-center">
 
-            <h1 class="logo me-auto"><a href="/">R2A Team</a></h1>
+            <h1 class="logo me-auto"><a href="/">Plagiarism Checker</a></h1>
 
             <nav id="navbar" class="navbar">
                 <ul>
